26e3d0c264c1e8c816a7a387efebf611e7c80535
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-17 21:43:48 +0530

    [Stock Processing Day11/day12]

---
+++ OOPS/Inventory.cs
@@ -0,0 +1,155 @@
+﻿
+namespace OOPS
+{
+    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+
+    
+    public class Inventory
+    {
+       
+        public void JsonDeserilaize()
+        {
+            InventoryManager manager = new InventoryManager();
+            
+            InventoryUtility utility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(@"C:\Users\Admin\source\repos\Saurabh\OOPS\OOPS\json.json"));
+            
+            Console.WriteLine("1. Display Inventory\n2. Add to Inventory\n3. Remove from Inventory\n4. Update to Inventory\nEnter your Option:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("1.Rice\n2.Pulse\n3.Wheats\nEnter your option:");
+                    int displayOption = Convert.ToInt32(Console.ReadLine());
+                    switch (displayOption)
+                    {
+                        case 1:
+                            // display inventory list of rice
+                            manager.DisplayInventory(utility.RiceList);
+                            break;
+                        case 2:
+                            // display inventory list of pulses
+                            manager.DisplayInventory(utility.PulseList);
+                            break;
+                        case 3:
+                            // display inventory list of wheat
+                            manager.DisplayInventory(utility.WheatsList);
+                            break;
+                        default:
+                            // switch case default message
+                            Console.WriteLine("Invalid Input!!!!!!!!!!");
+                            break;
+
+                    }
+                    break;
+                case 2:
+                    // options
+                    Console.WriteLine("\n1. Add Record to Rice\n2. Add Record to pulse\n3. Add Record to wheat\nEnter your Option:");
+                    int optionOne = Convert.ToInt32(Console.ReadLine());
+                    switch (optionOne)
+                    {
+                        case 1:
+                            // add records to inventory
+                            utility.RiceList = manager.AddToInventory(utility.RiceList);
+                            // update added record to json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        case 2:
+                            // add records to inventory
+                            utility.PulseList = manager.AddToInventory(utility.PulseList);
+                            // update added record to json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        case 3:
+                            // add records to inventory
+                            utility.WheatsList = manager.AddToInventory(utility.WheatsList);
+                            // update added record to json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        default:
+                            // switch case default message
+                            Console.WriteLine("Invalid Input!!!!!!!!!!");
+                            break;
+                    }
+                    break;
+                case 3:
+                    // options
+                    Console.WriteLine("\n1. remove Record from Rice\n2. remove Record from pulse\n3. remove Record from wheat\nEnter your Option:");
+                    int optionTwo = Convert.ToInt32(Console.ReadLine());
+                    switch (optionTwo)
+                    {
+                        case 1:
+                            // display inventory list
+                            manager.DisplayInventory(utility.RiceList);
+                            // remove record from inventory
+                            utility.RiceList = manager.RemoveFromInventory(utility.RiceList);
+                            // update json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        case 2:
+                            // display inventory list
+                            manager.DisplayInventory(utility.PulseList);
+                            // remove record from inventory
+                            utility.PulseList = manager.RemoveFromInventory(utility.PulseList);
+                            // update json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        case 3:
+                            // display inventory list
+                            manager.DisplayInventory(utility.WheatsList);
+                            // remove record from inventory
+                            utility.WheatsList = manager.RemoveFromInventory(utility.WheatsList);
+                            // update json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        default:
+                            // switch case default message
+                            Console.WriteLine("Invalid Input!!!!!!!!!!");
+                            break;
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("\n1. Update Record to Rice\n2. Update Record to pulse\n3. Update Record to wheat\nEnter your Option:");
+                    int optionThree = Convert.ToInt32(Console.ReadLine());
+                    switch (optionThree)
+                    {
+                        case 1:
+                            // display inventory list
+                            manager.DisplayInventory(utility.RiceList);
+                            // update a perticular record from a list
+                            utility.RiceList = manager.UpdateToInventory(utility.RiceList);
+                            // update json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        case 2:
+                            // display inventory list
+                            manager.DisplayInventory(utility.PulseList);
+                            // update a perticular record from a list
+                            utility.PulseList = manager.UpdateToInventory(utility.PulseList);
+                            // update json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        case 3:
+                            // display inventory list
+                            manager.DisplayInventory(utility.WheatsList);
+                            // update a perticular record from a list
+                            utility.WheatsList = manager.UpdateToInventory(utility.WheatsList);
+                            // update json file by serializing
+                            File.WriteAllText(@"F:\BL_.net278\Demo\OOps\OOPs\OOPS\json.json", JsonConvert.SerializeObject(utility));
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Input!!!!!!!!!!");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input!!!!!!!!!!");
+                    break;
+
+            }
+            
+
+        }
+    }
+}
---
+++ OOPS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("OOPS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOPS")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("056c6060-cd33-48ca-b437-ad8a2edad6e0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
